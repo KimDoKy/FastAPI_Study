fcbeddfdf23b7c157010310c6b1c0b3bf13c8cc9
DoKyung Kim <dokyungkim@DoKyungs-MacBook-Air.local>
2022-08-12 13:45:24 +0900

    나머지

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,10 +2,13 @@ from databases import DatabaseURL
 from starlette.config import Config
 from starlette.datastructures import Secret
 config = Config(".env")
+
 PROJECT_NAME = "phresh"
 VERSION = "1.0.0"
 API_PREFIX = "/api"
+
 SECRET_KEY = config("SECRET_KEY", cast=Secret, default="CHANGEME")
+
 POSTGRES_USER = config("POSTGRES_USER", cast=str)
 POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=Secret)
 POSTGRES_SERVER = config("POSTGRES_SERVER", cast=str, default="db")
