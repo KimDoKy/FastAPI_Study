f21db65e1621fe3ab0722564a605e62b979e5454
DoKyung Kim <dokyungkim@DoKyungs-MacBook-Air.local>
2022-08-13 20:47:15 +0900

    part4 / update database config

--- backend/app/db/migrations/env.py
+++ backend/app/db/migrations/env.py
@@ -1,7 +1,9 @@
 import pathlib
 import sys
+import os
 import alembic
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import engine_from_config, create_engine, pool
+from psycopg2 import DatabaseError
 
 from logging.config import fileConfig
 import logging
@@ -9,7 +11,7 @@ import logging
 # config를 쉽게 가져오기 위해 앱디렉터리를 추가
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[3]))
 
-from app.core.config import DATABASE_URL
+from app.core.config import DATABASE_URL, POSTGRES_DB
 
 # ini 파일의 값에 대한 액세스를 제공하는 Alembic Config 객체
 config = alembic.context.config
@@ -21,8 +23,15 @@ def run_migrations_online() -> None:
     """
     Run migrations in 'online' mode
     """
+    DB_URL = f"{DATABASE_URL}_test" if os.environ.get("TESTING") else str(DATABASE_URL)
+    if os.environ.get("TESTING"):
+        default_engine = create_engine(str(DATABASE_URL), isolation_level="AUTOCOMMIT")
+        with default_engine.connect() as default_conn:
+            default_conn.execute(f"DROP DATABASE IF EXISTS {POSTGRES_DB}_test")
+            default_conn.execute(f"CREATE DATABASE {POSTGRES_DB}_test")
+
     connectable = config.attributes.get("connection", None)
-    config.set_main_option("sqlalchemy.url", str(DATABASE_URL))
+    config.set_main_option("sqlalchemy.url", DB_URL)
 
     if connectable is None:
         connectable = engine_from_config(
@@ -43,6 +52,8 @@ def run_migrations_offline() -> None:
     """
     Run migrations in 'offline' mode
     """
+    if os.environ.get("TESTING"):
+        raise DatabaseError("Running testing migrations offline currently not permitted.")
     alembic.context.configure(url=str(DATABASE_URL))
     with alembic.context.begin_transaction():
         alembic.context.run_migrations()
